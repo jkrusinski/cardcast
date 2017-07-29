75ef055d6487d1f5cb2a46cff8b1c77f3203d191
Jerry Krusinski <jkruzr@gmail.com>
2017-07-29 13:40:00 -0500

    adds test for duplicate signups

--- test/users.test.js
+++ test/users.test.js
@@ -29,5 +29,24 @@ describe('Users', () => {
           expect(user.username).to.equal('bobby');
         });
     });
+
+    it('should not allow duplicate users to be created', () => {
+      const user = { username: 'bobby', password: 'secret' };
+      return users.create(user)
+        .then(() => {
+          return chai.request(server)
+            .post('/api/users/signup')
+            .send(user)
+            // make sure if the request is successful it is still sent to catch
+            .then(res => { throw res; });
+        })
+        .catch(err => {
+          expect(err).to.have.status(400);
+          return users.find({ username: 'bobby' });
+        })
+        .then(results => {
+          expect(results.length).to.eql(1);
+        });
+    });
   });
 });
