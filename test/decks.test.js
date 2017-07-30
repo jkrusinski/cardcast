97aacd9d1409f66935ce4442cae8c4f3aedb101e
Jerry Krusinski <jkruzr@gmail.com>
2017-07-30 14:44:35 -0500

    begins tests for decks

---
+++ test/decks.test.js
@@ -0,0 +1,101 @@
+process.env.NODE_ENV = 'test';
+
+const mongoose = require('mongoose');
+const Deck = require('../db/schemas/Deck.js');
+const User = require('../db/schemas/User.js');
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../server/server.js');
+const _ = require('lodash');
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+// persist cookies across the entire test
+const agent = chai.request.agent(server);
+
+const mockUser = {
+  username: 'bobby',
+  password: 'secret'
+};
+
+const mockDecks = [
+  {
+    title: 'Morning Stand Up',
+    description: 'A collection of notes for this morning\'s stand up'
+  },
+  {
+    title: 'Evening Stand Up',
+    description: 'A collection of notes for this evening\'s stand up'
+  }
+];
+
+
+before(() => {
+  // delete everything in db
+  Promise.all([User.remove({}), Deck.remove({})])
+    // create mock user
+    .then(() => {
+      return User.create(mockUser);
+    })
+    // add sample decks with user id
+    .then((user) => {
+      return Promise.all(mockDecks.map(deck => {
+        return Deck.create(Object.assign({}, deck, { user: user._id }));
+      }));
+    });
+});
+
+describe('Deck Routes', () => {
+  describe('GET /api/decks', () => {
+    afterEach(() => {
+      return agent.post('/api/users/logout');
+    });
+
+    it('should be a protected route', () => {
+      return agent.get('/api/decks')
+        .then(res => { throw res; })
+        .catch((err) => {
+          expect(err).to.have.status(401);
+        });
+    });
+
+    it('should return the logged in user\'s decks', () => {
+      return agent.post('/api/users/login')
+        .send(mockUser)
+        .then(() => {
+          return agent.get('/api/decks');
+        })
+        .then((res) => {
+          const userDecks = res.body;
+          expect(res).to.have.status(200);
+          mockDecks.forEach((deck) => {
+            expect(_.find(userDecks, deck)).to.exist;
+          });
+        });
+    });
+  });
+
+  describe('POST /api/decks', () => {
+    afterEach(() => {
+      return agent.post('/api/users/logout');
+    });
+
+    it('should be a protected route', () => {
+      return agent.post('/api/decks')
+        .send({
+          title: 'All-Hands Meeting',
+          description: 'A few slides to put up for the all-hands meeting'
+        })
+        .then((res) => { throw res; })
+        .catch((err) => {
+          expect(err).to.have.status(401);
+          return Deck.findOne({ title: 'All-Hands Meeting' });
+        })
+        .then((deck) => {
+          expect(deck).to.be.null;
+        });
+    });
+  });
+});
